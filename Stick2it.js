47f252cda315dc8170e9584c8e64f6dde890d073
Paul Gonzalez <robertgonzalez018@gmail.com>
2017-08-01 21:40:34 -0700

    final changes

--- Stick2it.js
+++ Stick2it.js
@@ -1,3 +1,13 @@
 // document.addEventListener('DOMContentLoaded', function() {
 //  var checkPageButton = document.getElementById('checkPage');
 //  checkPageButton.addEventListener('click', function() {
+var myApiCode = 'a4a08bf85a1842c58043ed9f49a688bd'
+var myUrl = 'https://newsapi.org/v1/articles?source=the-economist&sortBy=top&apiKey=a4a08bf85a1842c58043ed9f49a688bd'
+
+$.ajax({
+    method: "GET",
+    url: myUrl,
+    success: function(data) {
+      console.log(data);
+    }
+  });
